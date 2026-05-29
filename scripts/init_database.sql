1b4ba7981155698089ece705efce1e379e5cab26
Nyakuni992 <nyanetah@gmail.com>
2026-05-29 12:59:06 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,16 @@
+-- Create Database 'Northwind_Traders'
+
+USE master;
+
+CREATE DATABASE Northwind_Traders
+
+-- Create Schemas in Database
+
+USE Northwind_Traders;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
 
