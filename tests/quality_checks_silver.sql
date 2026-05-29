480a6109645dae5fb9f171060ad11e713dc545d7
Nyakuni992 <nyanetah@gmail.com>
2026-05-29 23:31:48 +0300

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -217,7 +217,7 @@ WHERE quantityPerUnit LIKE '%ounce%'
    OR quantityPerUnit LIKE '%packages%'
    OR quantityPerUnit LIKE '%bottles%'
     
-===========================
+--===========================
 -- Checking Shippers
 --=========================================================
 -- Check for unwanted spaces
